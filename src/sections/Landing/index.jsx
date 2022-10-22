eaf206c03bcb631290828244b2555e9e027fa932
Frederic Pun <frederic.pun@mail.utoronto.ca>
2022-10-22 14:51:19 -0400

    Update index.jsx

--- src/sections/Landing/index.jsx
+++ src/sections/Landing/index.jsx
@@ -10,7 +10,7 @@ export default function Landing() {
         Frederic Pun
       </Text>
       <Text type='subheading' color='primary'>
-        Associate Software Developer @ Konrad
+        Software Developer @ Konrad
       </Text>
     </Section>
   );
